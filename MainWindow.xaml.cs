164c8be92cf20c424d1ee2b8f21dca4536ce0f85
Benwt02 <chensfamily02@gmail.com>
2023-06-18 23:42:13 +0800

    程式碼撰寫

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,9 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Timer
 {
+
+
+
     /// <summary>
     /// MainWindow.xaml 的互動邏輯
     /// </summary>
@@ -23,6 +27,75 @@ namespace Timer
         public MainWindow()
         {
             InitializeComponent();
+
+            List<string> hours = new List<string>();
+            List<string> minutes = new List<string>();
+            DispatcherTimer timer = new DispatcherTimer();
+
+
+
+
+            // 建立小時的清單，數字範圍為00-23
+            for (int i = 0; i <= 23; i++)
+                hours.Add(string.Format("{0:00}", i));
+            // 建立分鐘的清單，數字範圍為00-59
+            for (int i = 0; i <= 59; i++)
+                minutes.Add(string.Format("{0:00}", i));
+
+            // 設定小時下拉選單的選單內容
+            cmbHour.ItemsSource = hours;
+            // 設定分鐘下拉選單的選單內容
+            cmbMin.ItemsSource = minutes;
+
+            timer.Interval = TimeSpan.FromSeconds(1);   // 這個計時器設定每一個刻度為1秒
+            timer.Tick += new EventHandler(timer_tick); // 每一個時間刻度設定一個小程序timer_tick
+            timer.Start(); // 啟動這個計時器
+
+
+
         }
+
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            txtTime.Text = DateTime.Now.ToString("HH:mm:ss");    // 顯示時間
+            txtDate.Text = DateTime.Now.ToString("yyyy-MM-dd");  // 顯示日期
+            txtWeekDay.Text = DateTime.Now.ToString("dddd");     // 顯示星期幾
+
+        }
+
+        
+
+        private void timerAlert_tick(object sender, EventArgs e)
+        {
+            // 判斷現在時間是不是已經是鬧鐘設定時間？如果時間到了，就要播放鬧鐘聲音
+            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
+            {
+                meSound.LoadedBehavior = MediaState.Play; // 開啟鬧鐘聲音
+                timerAlert.Stop(); // 停止鬧鐘計時器
+            }
+        }
+
+
+        private void btnSetAlert_Click(object sender, RoutedEventArgs e)
+        {
+            timerAlert.Start(); // 啟動鬧鐘計時器
+            btnSetAlert.IsEnabled = false;
+            btnCancelAlert.IsEnabled = true;
+            strSelectTime = cmbHour.SelectedItem + ":" + cmbMin.SelectedItem;
+        }
+
+
+
+        private void btnCancelAlert_Click(object sender, RoutedEventArgs e)
+        {
+            meSound.LoadedBehavior = MediaState.Stop; // 關閉鬧鐘聲音
+            timerAlert.Stop(); // 停止鬧鐘計時器
+            btnSetAlert.IsEnabled = true;
+            btnCancelAlert.IsEnabled = false;
+        }
+        string strSelectTime = "";
+        DispatcherTimer timerAlert = new DispatcherTimer();
+        
     }
 }
